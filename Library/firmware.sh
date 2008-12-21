12762dfadae0a8f450bc8fc25ad31384d4c096ef
Jay Freeman (saurik) <saurik@saurk.com>
2008-12-21 07:34:47 +0000

    Gracefully handle someone /deleting/ the firmware package.

--- Library/firmware.sh
+++ Library/firmware.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
+set -e
 
-# XXX: FIX THIS!!
-architecture=iphoneos-arm
 version=$(sw_vers -productVersion)
 
-if grep '^Package: firmware$' /var/lib/dpkg/status >/dev/null; then
-    cat /var/lib/dpkg/status | while IFS= read -r line; do
-        if [[ ${line} == 'Package: firmware' ]]; then
-            firmware=
-        elif [[ ${line} == '' ]]; then
-            unset firmware
-        elif [[ ${line} == Architecture:* && "${firmware+@}" ]]; then
-            echo "Architecture: ${architecture}"
-            continue
-        elif [[ ${line} == Version:* && "${firmware+@}" ]]; then
-            echo "Version: ${version}"
-            continue
-        fi
+cat /var/lib/dpkg/status | while IFS= read -r line; do
+    if [[ ${line} == 'Package: firmware' ]]; then
+        firmware=
+    elif [[ ${line} == '' ]]; then
+        unset firmware
+    elif [[ "${firmware+@}" ]]; then
+        continue
+    fi
 
-        echo "${line}"
-    done >/var/lib/dpkg/status_
-else
-    cat /var/lib/dpkg/status - >/var/lib/dpkg/status_ <<EOF
+    echo "${line}"
+done >/var/lib/dpkg/status_
+
+cat >>/var/lib/dpkg/status_ <<EOF
 Package: firmware
 Essential: yes
 Status: install ok installed
@@ -29,13 +23,14 @@ Priority: required
 Section: System
 Installed-Size: 0
 Maintainer: Jay Freeman (saurik) <saurik@saurik.com>
-Architecture: ${architecture}
+Architecture: iphoneos-arm
 Version: ${version}
 Description: almost impressive Apple frameworks
 Name: iPhone Firmware
 
 EOF
-fi && mv -f /var/lib/dpkg/status{_,}
+
+mv -f /var/lib/dpkg/status{_,}
 
 echo "/." >/var/lib/dpkg/info/firmware.list
 

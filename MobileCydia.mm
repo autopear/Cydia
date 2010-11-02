2ecf1e7f1f14de505d5e7b374c0feba96a62126e
Jay Freeman (saurik) <saurik@saurik.com>
2010-11-02 06:00:58 -0700

    Put off the no-title-on-HomeController UI update.

--- MobileCydia.mm
+++ MobileCydia.mm
@@ -6347,12 +6347,12 @@ bool DepSubstrate(const pkgCache::VerIterator &iterator) {
 
 - (void) viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    [[self navigationController] setNavigationBarHidden:YES animated:animated];
+    //[[self navigationController] setNavigationBarHidden:YES animated:animated];
 }
 
 - (void) viewWillDisappear:(BOOL)animated {
     [super viewWillDisappear:animated];
-    [[self navigationController] setNavigationBarHidden:NO animated:animated];
+    //[[self navigationController] setNavigationBarHidden:NO animated:animated];
 }
 
 - (id) init {

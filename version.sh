88006baecbc7e5bd98f973ef25cf5dd8e878e8b4
Jay Freeman (saurik) <saurik@saurik.com>
2010-12-01 07:03:18 -0800

    Add a ~srk to the version if !ForRelease.

--- version.sh
+++ version.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-git describe --tags --dirty="+" --match="v*" | sed -e 's@-\([^-]*\)-\([^-]*\)$@+\1.\2@;s@^v@@'
+echo -n "$(git describe --tags --dirty="+" --match="v*" | sed -e 's@-\([^-]*\)-\([^-]*\)$@+\1.\2@;s@^v@@')"
+grep '#define ForRelease 0' MobileCydia.mm &>/dev/null && echo -n '~srk'
+echo

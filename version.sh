36eaf7cfca80222301f5c8cca88086dd696871d7
Jay Freeman (saurik) <saurik@saurik.com>
2010-12-06 03:04:40 -0800

    Allow ./version.sh on a specified commit-ish.

--- version.sh
+++ version.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo -n "$(git describe --tags --dirty="+" --match="v*" | sed -e 's@-\([^-]*\)-\([^-]*\)$@+\1.\2@;s@^v@@')"
+
+if [[ $# -eq 0 ]]; then
+    flags=(--dirty="+")
+else
+    flags=("$@")
+fi
+
+echo -n "$(git describe --tags --match="v*" "${flags[@]}" | sed -e 's@-\([^-]*\)-\([^-]*\)$@+\1.\2@;s@^v@@')"
 grep '#define ForRelease 0' MobileCydia.mm &>/dev/null && echo -n '~srk'
 echo

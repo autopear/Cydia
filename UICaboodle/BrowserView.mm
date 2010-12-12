2afa199a04ebf48735f7cd20aafc94bc958ea4df
Grant Paul <chpwn@chpwn.com>
2010-12-11 20:42:01 -0800

    Undo my own fix for the lack of pinstripes on the iPad: pinstripes spread out when rotating when using -pinStripeColor. A real fix is still needed.

--- UICaboodle/BrowserView.mm
+++ UICaboodle/BrowserView.mm
@@ -715,7 +715,7 @@ static void $UIWebViewWebViewDelegate$webViewClose$(UIWebViewWebViewDelegate *se
                             UIColor *uic(nil);
 
                             if (red == 0xc7 && green == 0xce && blue == 0xd5)
-                                uic = [UIColor pinStripeColor];
+                                uic = [UIColor groupTableViewBackgroundColor];
                             else if (alpha != 0)
                                 uic = [UIColor
                                     colorWithRed:(red / 255)
@@ -732,7 +732,7 @@ static void $UIWebViewWebViewDelegate$webViewClose$(UIWebViewWebViewDelegate *se
                     }
 
                     if (!colored)
-                        [scroller_ setBackgroundColor:[UIColor pinStripeColor]];
+                        [scroller_ setBackgroundColor:[UIColor groupTableViewBackgroundColor]];
                     break;
                 }
     }
@@ -995,7 +995,7 @@ static void $UIWebViewWebViewDelegate$webViewClose$(UIWebViewWebViewDelegate *se
         }
 
         [scroller_ setFixedBackgroundPattern:YES];
-        [scroller_ setBackgroundColor:[UIColor pinStripeColor]];
+        [scroller_ setBackgroundColor:[UIColor groupTableViewBackgroundColor]];
         [scroller_ setClipsSubviews:YES];
 
         [scroller_ setBounces:YES];

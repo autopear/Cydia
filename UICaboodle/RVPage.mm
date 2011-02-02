ad23ca68f69f54bfbf622664690c9ba9a72bb617
Grant Paul <chpwn@chpwn.com>
2011-02-02 00:11:55 -0800

    Added session persistence; moved -init to -loadView, -viewDidLoad, etc; added releasing of views in low-memory situations; style changes.

--- UICaboodle/RVPage.mm
+++ UICaboodle/RVPage.mm
@@ -6,12 +6,39 @@
 #import "RVBook.h"
 
 @implementation CYViewController
+
 - (void) setDelegate:(id)delegate {
     delegate_ = delegate;
 }
-- (void) reloadData {
+
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+
+    if (!loaded_)
+        [self reloadData];
 }
+
+- (void) releaseSubviews {
+    // Do nothing.
+}
+
+- (void) setView:(UIView *)view {
+    if (view == nil)
+        [self releaseSubviews];
+
+    [super setView:view];
+}
+
+- (void) reloadData {
+    loaded_ = YES;
+}
+
+- (NSURL *) navigationURL {
+    return nil;
+}
+
 - (BOOL) shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)orientation {
     return (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad || orientation == UIInterfaceOrientationPortrait);
 }
+
 @end

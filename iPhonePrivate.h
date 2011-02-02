817748f003e0ee681ea0a35c20852940c117ce6e
Jay Freeman (saurik) <saurik@saurik.com>
2011-02-01 23:46:05 -0800

    We can't use 3.0 constants on 2.0 SDK.

--- iPhonePrivate.h
+++ iPhonePrivate.h
@@ -354,6 +354,7 @@ extern float const UIScrollViewDecelerationRateNormal;
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < 30000
 
 #define kSCNetworkReachabilityFlagsConnectionOnDemand (1 << 5)
+#define kCFCoreFoundationVersionNumber_iPhoneOS_3_0 478.47
 
 #endif
 // }}}
